c89d9e6094a366d7f637ad4f2dfddac83a41766d
BruSeLY <103708209+BruSeLY@users.noreply.github.com>
2024-01-06 13:15:16 +0900

    constructor

--- create_level.py
+++ create_level.py
@@ -25,7 +25,7 @@ class Camera:
 
 class Block(pygame.sprite.Sprite):
     def __init__(self, block_type, pos_x, pos_y):
-        super().__init__(block_group, all_sprites)
+        super().__init__(all_sprites)
         self.image = block_images[block_type]  # строка с названием
         self.rect = self.image.get_rect().move(
             pos_x, pos_y)
@@ -37,28 +37,22 @@ block_names = ([f"{name} Big" for name in ["Autumn", "Fantasy", "Grass", "Jade",
                [f"{name} Big" for name in ["Autumn", "Fantasy", "Grass", "Jade", "Stone", "Wood"]])
 block_images = {key: load_image(rf"Terrain\Square Blocks\{key}.png") for key in block_names}
 
-print(block_names)
-
 # Функция отрисовки уровня
 
 
 
 # Создание уровня
-level = pygame.sprite.Group()
 level_save = []
 selected_block = None
 
 
-
-
-
 # Главный цикл игры
 def create_level():
-    global selected_block, level, level_save
+    global selected_block, level_save
     gen_background()
     camera = Camera()
+    buttons = pygame.sprite.Group()
     blocks = pygame.sprite.Group()
-    observer = pygame.sprite.Group()
     block_sur = []
     save_button = pygame.sprite.Sprite()
     eye = pygame.sprite.Sprite()
@@ -67,7 +61,7 @@ def create_level():
     save_button.image = pygame.transform.scale(save_button.image, (60, 60))
     save_rect = pygame.Rect(WIDTH - 100, HEIGHT - 105, 100, 100)
     save_button.rect = save_rect
-    all_sprites.add(save_button)
+    buttons.add(save_button)
     block_name = []
 
     eye.image = load_image(rf"menu\buttons\play.png")
@@ -90,10 +84,15 @@ def create_level():
                 if event.key == pygame.K_RIGHT:
                     # Передвижение вправо
                     camera.dx -= 1
-
                 elif event.key == pygame.K_LEFT:
                     # Передвижение влево
                     camera.dx += 1
+                elif event.key == pygame.K_UP:
+                    # Передвижение вверх
+                    camera.dy += 1
+                elif event.key == pygame.K_DOWN:
+                    # Передвижение вниз
+                    camera.dy -= 1
 
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:  # Левая кнопка мыши
@@ -108,28 +107,26 @@ def create_level():
                             selected_block = block_names[curr]
                             break
                         curr += 1
-                    else:
-                        # Проверяем, что выбранный блок не ниже верхней точки оставшихся блоков
-                        if selected_block is not None and round(y / 50) * 48 < 550:
-                            x = round(x / 50) * 48
-                            y = round(y / 50) * 48
-                            level.add(Block(selected_block, x, y))
+
+                    else:    # Проверяем, что выбранный блок не ниже верхней точки оставшихся блоков
+                        if selected_block is not None and y < 550:
+                            block_group.add(Block(selected_block, x, y))
                             level_save.append((selected_block, x, y))
-                            selected_block = None
+
                 elif event.button == 3:  # Правая кнопка мыши
                     x, y = event.pos
-                    for block in level:
+                    for block in block_group:
                         if block.rect.collidepoint(x, y):
-                            level.remove(block)
+                            block_group.remove(block)
                             all_sprites.remove(block)
                             break
 
         screen.fill(pygame.Color("orange"))
         all_sprites.draw(screen)
-        level.draw(screen)
-        blocks.draw(screen)
-        for sprite in all_sprites:
+        block_group.draw(screen)
+        for sprite in block_group:
             camera.apply(sprite)
+        camera.apply(save_button)
         pygame.display.flip()
         clock.tick(FPS)
 
