33f975b81249aa79b921fda7bb23322fa9a32d9a
BruSeLY <103708209+BruSeLY@users.noreply.github.com>
2024-01-17 19:55:11 +0900

    constructor

--- create_level.py
+++ create_level.py
@@ -4,6 +4,8 @@ from background import animate_background, tiles, load_image, background, gen_ba
 import os
 
 start_point = pygame.Rect(0, HEIGHT, 1, 1)
+blocks = pygame.sprite.Group()
+
 
 
 class Camera:
@@ -23,9 +25,23 @@ class Camera:
         start_point.y += self.dy
 
 
-class Block(pygame.sprite.Sprite):
+camera = Camera()
+
+
+class Button(pygame.sprite.Sprite):
     def __init__(self, block_type, pos_x, pos_y):
-        super().__init__(all_sprites)
+        super().__init__(blocks)
+        self.image = block_images[block_type]  # строка с названием
+        self.rect = self.image.get_rect().move(
+            pos_x, pos_y)
+
+    def scroll(self, vector):
+        self.rect = self.rect.move(vector, 0)
+
+
+class Unit(pygame.sprite.Sprite):
+    def __init__(self, block_type, pos_x, pos_y):
+        super().__init__(block_group)
         self.image = block_images[block_type]  # строка с названием
         self.rect = self.image.get_rect().move(
             pos_x, pos_y)
@@ -34,7 +50,7 @@ class Block(pygame.sprite.Sprite):
 hero_group = pygame.sprite.Group()
 block_group = pygame.sprite.Group()
 block_names = ([f"{name} Big" for name in ["Autumn", "Fantasy", "Grass", "Jade", "Stone", "Wood"]] +
-               [f"{name} Big" for name in ["Autumn", "Fantasy", "Grass", "Jade", "Stone", "Wood"]])
+               [f"{name} Small" for name in ["Autumn", "Fantasy", "Grass", "Jade", "Stone", "Wood"]])
 block_images = {key: load_image(rf"Terrain\Square Blocks\{key}.png") for key in block_names}
 
 # Функция отрисовки уровня
@@ -48,21 +64,54 @@ selected_block = None
 
 # Главный цикл игры
 def create_level():
-    global selected_block, level_save
+    global selected_block, level_save, blocks, camera
     gen_background()
-    camera = Camera()
+    curr_block = 1
     buttons = pygame.sprite.Group()
     blocks = pygame.sprite.Group()
+    blocks_small = pygame.sprite.Group()
     block_sur = []
     save_button = pygame.sprite.Sprite()
     eye = pygame.sprite.Sprite()
+    next = pygame.sprite.Sprite()
+    prev = pygame.sprite.Sprite()
     quit_button = pygame.sprite.Sprite()
     surf = pygame.Surface((800, 200))  # при создании передается размер
     surf.fill((255, 255, 255))
+    fon = pygame.Surface((800, 600))
+    fon.fill(pygame.Color("orange"))
+    block_sur = []
+    block_name = []
+
+    for i in range(6):
+        block = [Button(block_names[i], (800 - 50 * 7) // 2 + i * 55, 525), block_names[i]]
+        block_sur.append(block[0])
+        block_name.append(block[1])
+        blocks.draw(screen)
+
+    for i in range(6):
+        block = [Button(block_names[i + 6], (800 - 48 * 7) // 2 + i * 55 + 800, 535), block_names[i + 6]]
+        block_sur.append(block[0])
+        block_name.append(block[1])
+        blocks.draw(screen)
+
+    next.image = load_image(rf"menu\buttons\play.png")
+    next.image = pygame.transform.scale(next.image, (50, 50))
+    next_rect = pygame.Rect(575, 525, 100, 100)
+    next.rect = next_rect
+    buttons.add(next)
+
+    prev.image = load_image(rf"menu\buttons\play.png")
+    prev.image = pygame.transform.scale(next.image, (50, 50))
+    prev.image = pygame.transform.rotate(next.image, 180)
+    prev_rect = pygame.Rect(150, 525, 50, 50)
+    prev.rect = prev_rect
+    buttons.add(prev)
 
     save_button.image = load_image(rf"menu\buttons\save_button.png")
-    save_button.image = pygame.transform.scale(save_button.image, (60, 60))
-    save_rect = pygame.Rect(WIDTH - 100, HEIGHT - 100, 100, 100)
+    save_button.image = pygame.transform.scale(save_button.image, (100, 50))
+    save_button.image.set_colorkey(save_button.image.get_at((0, 0)))
+    save_rect = pygame.Rect(WIDTH - 125, HEIGHT - 75, 100, 100)
     save_button.rect = save_rect
     buttons.add(save_button)
     block_name = []
@@ -72,18 +121,10 @@ def create_level():
     quit_rect = pygame.Rect(0, 0, 100, 100)
     quit_button.rect = quit_rect
     buttons.add(quit_button)
+    buttons_group = []
 
-    eye.image = load_image(rf"menu\buttons\play.png")
-    eye.image = pygame.transform.scale(save_button.image, (60, 60))
-    eye_rect = pygame.Rect(WIDTH - 100, HEIGHT - 105, 100, 100)
-    eye.rect = eye_rect
-    all_sprites.add(eye)
 
-    for i in range(6):
-        block = [Block(block_names[i], (800 - 50 * 7) // 2 + i * 55, 525), block_names[i]]
-        block_sur.append(block[0])
-        block_name.append(block[1])
-        blocks.add(block_sur)
+    curr_page = 1
 
     while True:
         for event in pygame.event.get():
@@ -115,25 +156,36 @@ def create_level():
                         save_level()
                         return
 
+                    if next_rect.collidepoint(event.pos):
+                        for sprite in block_sur:
+                            sprite.scroll(-WIDTH)
+
+                    if prev_rect.collidepoint(event.pos):
+                        for sprite in block_sur:
+                            sprite.scroll(WIDTH)
+
                     for block in blocks: # Проверяем, был ли клик на блоке
                         if block.rect.collidepoint(x, y):
                             selected_block = block_names[curr]
+                            select = True
                             break
                         curr += 1
 
-                    else:    # Проверяем, что выбранный блок не ниже верхней точки оставшихся блоков
-                        if selected_block is not None and y < 550:
+                    else: # Проверяем, что выбранный блок не ниже верхней точки оставшихся блоков
+                        if selected_block is not None and y < 500:
                             # Проверяем пересечение с уже существующими блоками
                             is_intersecting = False
-                            new_block_rect = pygame.Rect(x, y, 40, 40)
+                            if selected_block.split()[-1] == "Small":
+                                new_block_rect = pygame.Rect(x, y, 30, 30)
+                            else:
+                                new_block_rect = pygame.Rect(x, y, 40, 40)
                             for block in block_group:
                                 if block.rect.colliderect(new_block_rect):
                                     is_intersecting = True
                                     break
-
                             if not is_intersecting:
-                                block_group.add(Block(selected_block, x, y))
-                                level_save.append((selected_block, x, y))
+                                block_group.add(Unit(selected_block, x, y))
+                                level_save.append(Unit(selected_block, x, y))
 
                 elif event.button == 3:  # Правая кнопка мыши
                     x, y = event.pos
@@ -142,16 +194,14 @@ def create_level():
                             block_group.remove(block)
                             all_sprites.remove(block)
                             break
-
-        screen.fill(pygame.Color("orange"))
-
+        screen.blit(fon, (0, 0))
         block_group.draw(screen)
         for sprite in block_group:
             camera.apply(sprite)
+
         screen.blit(surf, (0, 500))
         blocks.draw(screen)
         buttons.draw(screen)
-
         pygame.display.flip()
         clock.tick(FPS)
 
