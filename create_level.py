2239fb3c41d8658ab6c425114f322eabb4246e39
BruSeLY <103708209+BruSeLY@users.noreply.github.com>
2024-01-28 20:52:06 +0900

    constructor

--- create_level.py
+++ create_level.py
@@ -1,10 +1,18 @@
 import pygame
-from settings import clock, screen, FPS, WIDTH, HEIGHT, all_sprites, CONSTUCTOR_CAMERA_X, CONSTUCTOR_CAMERA_Y
+from settings import clock, screen, FPS, WIDTH, HEIGHT, all_sprites, CONSTUCTOR_CAMERA_X, \
+    CONSTUCTOR_CAMERA_Y, TRAPS_PARAMETES
 from background import animate_background, tiles, load_image, background, gen_background
 from tkinter import Tk
 import os
+import pygame_gui
+
+# Создание менеджера интерфейса pygame_gui
+manager = pygame_gui.UIManager((800, 600))
+
+# Создание поля ввода для каждого параметра ловушки
 
 
+input_fields = []
 blocks = pygame.sprite.Group()
 start_point = pygame.Rect(0, HEIGHT, 1, 1)
 monitor_info = Tk()
@@ -58,14 +66,14 @@ camera = Camera()
 class Button(pygame.sprite.Sprite):
     def __init__(self, sprite_type, pos_x, pos_y, isBlock, withBack):
         super().__init__(blocks)
-
+        self.sprite_type = sprite_type
         if isBlock:
-            self.image = block_images[sprite_type]  # строка с названием
+            self.image = block_images[sprite_type] # строка с названием
         else:
             self.image = trap_images[sprite_type]
         if withBack:
             self.image.set_colorkey(self.image.get_at((0, 0)))
-
+        self.isBlock = isBlock
         self.rect = self.image.get_rect()
         self.rect.centerx = pos_x + 30
         self.rect.centery = pos_y + 100
@@ -75,16 +83,46 @@ class Button(pygame.sprite.Sprite):
 
 
 class Unit(pygame.sprite.Sprite):
-    def __init__(self, block_type, pos_x, pos_y, isBlock):
+    def __init__(self, block_type, pos_x, pos_y, isBlock, parameter=None):
         super().__init__(block_group)
         if isBlock:
             self.image = block_images[block_type]  # строка с названием
         else:
+            self.parameters = parameter.copy()  # Создание копии словаря параметров
+            print(self.parameters)
             self.image = trap_images[block_type]
+        self.isBlock = isBlock
         self.block_type = block_type
-
         self.rect = self.image.get_rect().move(
             pos_x, pos_y)
+
+    def create_input_fields(self):
+        # Очистка списка input_fields
+        input_fields.clear()
+
+        # Создание менеджера интерфейса pygame_gui
+        # Создание поля ввода для каждого параметра ловушки
+        marginTop = 10
+        for key, value in self.parameters.items():
+            input_field_rect = pygame.Rect(100, marginTop, 200, 30)
+            input_field = pygame_gui.elements.UITextEntryLine(input_field_rect, manager=manager)
+            input_field.set_text(str(value))
+            input_fields.append(input_field)
+            marginTop += 30
+        # Основной игровой цикл
+
+    def save_parameter(self):
+        curr = 0
+        for key, value in self.parameters.items():
+
+            self.parameters[key] = input_fields[curr].get_text()
+            print(self.parameters[key])
+            if self.parameters[key].isdigit():
+                self.parameters[key] = int(self.parameters[key])
+            elif "(" in self.parameters[key] and ")" in self.parameters[key]:
+                self.parameters[key] = eval(self.parameters[key])
+            curr += 1
+        print(self.parameters)
 
 
 hero_group = pygame.sprite.Group()
@@ -137,13 +175,9 @@ def create_level():
     global selected_block, level_save, blocks, camera
     gen_background()
     names_blocks_n_traps = block_names + folders
-    curr_block = 1
     buttons = pygame.sprite.Group()
     blocks = pygame.sprite.Group()
-    blocks_small = pygame.sprite.Group()
-    block_sur = []
     save_button = pygame.sprite.Sprite()
-    eye = pygame.sprite.Sprite()
     next = pygame.sprite.Sprite()
     prev = pygame.sprite.Sprite()
     quit_button = pygame.sprite.Sprite()
@@ -153,6 +187,7 @@ def create_level():
     fon.fill(pygame.Color("orange"))
     block_sur = []
     block_name = []
+    can_add = True
 
     next.image = load_image(rf"menu\buttons\play.png")
     next.image = pygame.transform.scale(next.image, (50, 50))
@@ -166,8 +201,6 @@ def create_level():
     prev_rect = pygame.Rect(150, 525, 50, 50)
     prev.rect = prev_rect
     buttons.add(prev)
-
-
 
     current_screen = 0
     block_x = 50
@@ -197,34 +230,34 @@ def create_level():
         block_name.append(block[1])
         blocks.draw(screen)
 
-
-
     save_button.image = load_image(rf"menu\buttons\save.png")
     save_button.image = pygame.transform.scale(save_button.image, (100, 50))
     save_button.image.set_colorkey(save_button.image.get_at((0, 0)))
     save_rect = pygame.Rect(WIDTH - 125, HEIGHT - 75, 100, 100)
     save_button.rect = save_rect
     buttons.add(save_button)
-    block_name = []
 
     quit_button.image = load_image(rf"menu\buttons\close.png")
     quit_button.image = pygame.transform.scale(quit_button.image, (50, 50))
     quit_rect = pygame.Rect(0, 0, 100, 100)
     quit_button.rect = quit_rect
     buttons.add(quit_button)
-    buttons_group = []
-
-
-    curr_page = 1
 
     while True:
         camera.apply_mouse()
         for event in pygame.event.get():
+            manager.process_events(event)
             if event.type == pygame.QUIT:
                 return 0
-
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_s:
+                    for block in block_group:
+                        if block.rect.collidepoint(parameter_change_coord) and not block.isBlock:
+                            block.save_parameter()
+                            for field in input_fields:
+                                field.kill()
+                            can_add = True
             elif event.type == pygame.MOUSEBUTTONDOWN:
-
                 if event.button == 1:  # Левая кнопка мыши
                     x, y = event.pos
                     curr = 0
@@ -245,11 +278,14 @@ def create_level():
                     for block in blocks: # Проверяем, был ли клик на блоке
                         if block.rect.collidepoint(x, y):
                             selected_block = names_blocks_n_traps[curr]
+                            if "idle.png" in selected_block:
+                                selected_block_type = "Trap"
+                            else:
+                                selected_block_type = "Block"
                             break
                         curr += 1
-
                     else:
-                        if selected_block is not None:
+                        if selected_block is not None and can_add is True:
                             isBlock = True
                             width_block, height_block = 0, 0
                             if r"idle.png" in selected_block:
@@ -259,9 +295,6 @@ def create_level():
                             else:
                                 rect = load_image(f'Terrain\Square Blocks\{selected_block}.png')
                                 width_block, height_block = rect.get_rect().width, rect.get_rect().height
-
-
-
 
                             new_block_rect = pygame.Rect(x, y, width_block, height_block)
 
@@ -274,9 +307,25 @@ def create_level():
                                     break
                             else:
                                 alligned_x, alligned_y = new_block_rect.x, new_block_rect.y
-                                new_unit = Unit(selected_block, alligned_x, alligned_y, isBlock)
+                                if selected_block_type == "Trap":
+                                    parameter = TRAPS_PARAMETES[selected_block]
+                                    new_unit = Unit(selected_block, alligned_x, alligned_y,
+                                                isBlock, parameter)
+                                else:
+                                    new_unit = Unit(selected_block, alligned_x, alligned_y,
+                                                    isBlock)
                                 block_group.add(new_unit)
                                 level_save.append(new_unit)
+
+                elif event.button == 2:
+                    can_add = False
+                    # Проверка, является ли блок типа ловушки
+                    for block in block_group:
+                        if block.rect.collidepoint(event.pos) and not block.isBlock:
+                            for field in input_fields:
+                                field.kill()
+                            parameter_change_coord = event.pos
+                            block.create_input_fields()
                 elif event.button == 3:  # Правая кнопка мыши
                     x, y = event.pos
                     for block in block_group:
@@ -284,7 +333,13 @@ def create_level():
                             level_save.pop(level_save.index(block))
                             block.kill()
                             break
+
+
         screen.blit(fon, (0, 0))
+        manager.draw_ui(screen)
+        # Обновление интерфейса pygame_gui
+        manager.update(1/60)
+        # Отрисовка интерфейса pygame_gui
         block_group.draw(screen)
         for sprite in block_group:
             camera.apply(sprite)
